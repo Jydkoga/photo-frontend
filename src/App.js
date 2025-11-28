757bbc048d27580d3220ea228de03456722e404e
James Koga <jyk54@cornell.edu>
2025-11-28 15:36:02 -0800

    Connect to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("https://photo-backend-u62f.onrender.com//")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error(err);
+        setMessage("Error connecting to backend");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Photo App</h1>
+      <p>Backend says: {message}</p>
     </div>
   );
 }
